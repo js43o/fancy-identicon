eb821128df2d3a637af202c2b7fae19c3bcae620
Jiseung <js43o@naver.com>
2023-12-28 21:05:49 +0900

    ✨ 입력값에 따라 랜덤 아이콘 생성 및 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Username } from '~/components';
+import md5 from 'crypto-js/md5';
 
 function App() {
   const [username, setUsername] = useState('');
+  const [blocks, setBlocks] = useState([...Array(15).map(() => 0)]);
 
   const onChangeUsername: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setUsername(e.target.value);
   };
+
+  useEffect(() => {
+    if (!username) {
+      setBlocks([...Array(15).map(() => 0)]);
+      return;
+    }
+
+    const hash = Array.from(md5(username).toString());
+    setBlocks(
+      hash
+        .slice(0, 15)
+        .map((char, idx) => char.charCodeAt(0) + hash[idx + 15].charCodeAt(0))
+        .map((char) => char % 2)
+    );
+  }, [username]);
 
   return (
     <div className="flex h-full w-full flex-col bg-slate-800 font-pretendard text-gray-50">
@@ -20,8 +37,16 @@ function App() {
         </a>
       </header>
       <main className="flex grow flex-col items-center justify-center gap-2">
-        <section className="flex h-1/2 w-1/2 items-center justify-center">
-          [image]
+        <section className="grid h-80 w-80 grid-flow-col grid-cols-5 grid-rows-5">
+          {[...blocks, ...blocks.slice(5, 10), ...blocks.slice(0, 5)].map(
+            (block, index) => (
+              <div
+                key={index}
+                className={`h-full w-full${block ? ' bg-gray-50' : ''}`}
+              />
+            )
+          )}
+          {}
         </section>
         <Username username={username} onChangeUsername={onChangeUsername} />
       </main>
