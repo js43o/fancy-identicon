2cc40c3baf14c4cf053eb680d78efe0177084428
Jiseung <js43o@naver.com>
2023-12-28 14:13:24 +0900

    💄 Pretendard 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      pretendard: 'Pretendard',
+    },
   },
   plugins: [],
 };
