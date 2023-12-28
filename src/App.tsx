018e2327b946fef156158c12b9a9b8409a951cd8
Jiseung <js43o@naver.com>
2023-12-28 15:02:46 +0900

    💄 기본 레이아웃 작성 및 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex h-full w-full flex-col bg-slate-800 font-pretendard text-gray-50">
+      <header className="flex justify-between p-4 ">
+        <div className="flex items-center gap-1">
+          <span className="material-symbols-outlined">deployed_code</span>
+          <span className="font-neoDunggeunmo">FancyIdenticon</span>
+        </div>
+        <a href="https://github.com/js43o/fancy-identicon">
+          <img src="/github.svg" alt="github" width="24" height="24" />
+        </a>
+      </header>
+      <main className="grow"></main>
+      <footer className="flex items-center justify-center p-4 text-gray-500">
+        made by js43o
+      </footer>
+    </div>
+  );
 }
 
 export default App;
