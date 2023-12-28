af6593b76d3d1ab1c164ae1f9340a4beefde368e
Jiseung <js43o@naver.com>
2023-12-28 21:53:20 +0900

    ✨ 입력값에 따라 랜덤 색상 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import md5 from 'crypto-js/md5';
 function App() {
   const [username, setUsername] = useState('');
   const [blocks, setBlocks] = useState([...Array(15).map(() => 0)]);
+  const [color, setColor] = useState([0, 0, 0]);
 
   const onChangeUsername: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setUsername(e.target.value);
@@ -16,14 +17,30 @@ function App() {
       return;
     }
 
-    const hash = Array.from(md5(username).toString());
+    const hash = md5(username).toString();
+    console.log(hash);
+
+    setColor([
+      parseInt(hash.slice(0, 16), 16) % 360,
+      (parseInt(hash.slice(16, 24), 16) % 40) + 30,
+      (parseInt(hash.slice(24, 32), 16) % 20) + 70,
+    ]);
     setBlocks(
-      hash
+      Array.from(hash)
         .slice(0, 15)
         .map((char, idx) => char.charCodeAt(0) + hash[idx + 15].charCodeAt(0))
         .map((char) => char % 2)
     );
   }, [username]);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      '--block-color',
+      `hsl(${color[0]},
+      ${color[1]}%,
+      ${color[2]}%)`
+    );
+  }, [color]);
 
   return (
     <div className="flex h-full w-full flex-col bg-slate-800 font-pretendard text-gray-50">
@@ -42,11 +59,12 @@ function App() {
             (block, index) => (
               <div
                 key={index}
-                className={`h-full w-full${block ? ' bg-gray-50' : ''}`}
+                className={`h-full w-full${
+                  block ? ` bg-[var(--block-color)]` : ''
+                }`}
               />
             )
           )}
-          {}
         </section>
         <Username username={username} onChangeUsername={onChangeUsername} />
       </main>
