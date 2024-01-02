db3a28be09b5067a0d4436c80b58e510d918b43b
Jiseung <js43o@naver.com>
2024-01-02 17:59:01 +0900

    💄 블록 크기 미세 조정

--- src/components/Identicon.tsx
+++ src/components/Identicon.tsx
@@ -22,21 +22,19 @@ export function Identicon({ blocks, glow, theme }: IdenticonProps) {
         }`}
       >
         <div
-          className={`absolute grid h-full w-full grid-flow-col grid-cols-5 grid-rows-5 p-8`}
+          className={`block-wrapper absolute grid h-full w-full grid-flow-col grid-cols-5 grid-rows-5 p-8`}
         >
           {[...blocks, ...blocks.slice(5, 10), ...blocks.slice(0, 5)].map(
-            (block, index) => (
-              <div
-                key={index}
-                className={`block border ${
-                  block
-                    ? `border-[var(--block-color)] bg-[var(--block-color)]`
-                    : 'border-transparent'
-                } ${
-                  glow ? 'drop-shadow-[0_0_0.75rem_var(--block-color)]' : ''
-                }`}
-              />
-            )
+            (block, index) =>
+              block ? (
+                <div
+                  key={index}
+                  className={`block border border-[var(--block-color)] bg-[var(--block-color)]
+               ${glow ? 'drop-shadow-[0_0_0.75rem_var(--block-color)]' : ''}`}
+                />
+              ) : (
+                <div key={index} />
+              )
           )}
         </div>
       </div>
