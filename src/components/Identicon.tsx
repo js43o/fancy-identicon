a96cc7dc99c0cd91825e31b9e2c6e7478ca1837a
Jiseung <js43o@naver.com>
2024-01-02 11:34:25 +0900

    💄 가장자리 패딩 추가

--- src/components/Identicon.tsx
+++ src/components/Identicon.tsx
@@ -15,22 +15,28 @@ export function Identicon({ blocks, glow, theme }: IdenticonProps) {
         className={`cube-top absolute h-full w-full bg-gray-200 transition-colors`}
       />
       <div
-        className={`cube-front ${
+        className={`cube-front relative h-full w-full transition-colors ${
           theme === 'dark' ? 'bg-gray-800' : 'bg-github-default'
-        } absolute z-10 grid h-full w-full grid-flow-col grid-cols-5 grid-rows-5 transition-colors`}
+        }`}
       >
-        {[...blocks, ...blocks.slice(5, 10), ...blocks.slice(0, 5)].map(
-          (block, index) => (
-            <div
-              key={index}
-              className={`block h-full w-full border ${
-                block
-                  ? `border-[var(--block-color)] bg-[var(--block-color)]`
-                  : 'border-transparent'
-              } ${glow ? 'drop-shadow-[0_0_0.75rem_var(--block-color)]' : ''}`}
-            />
-          )
-        )}
+        <div
+          className={`absolute grid h-full w-full grid-flow-col grid-cols-5 grid-rows-5 p-8`}
+        >
+          {[...blocks, ...blocks.slice(5, 10), ...blocks.slice(0, 5)].map(
+            (block, index) => (
+              <div
+                key={index}
+                className={`block border ${
+                  block
+                    ? `border-[var(--block-color)] bg-[var(--block-color)]`
+                    : 'border-transparent'
+                } ${
+                  glow ? 'drop-shadow-[0_0_0.75rem_var(--block-color)]' : ''
+                }`}
+              />
+            )
+          )}
+        </div>
       </div>
       <div
         className={`cube-side absolute h-full w-full bg-gray-400 transition-colors`}
